2eab7ac56df8d839b99dd96b29de461470a34cc7
Victor Lebrun <victor.lebrun011@gmail.com>
2022-01-20 15:36:07 +0100

    debug not credited users

--- api/src/pcapi/scripts/force_19yo_dms_import.py
+++ api/src/pcapi/scripts/force_19yo_dms_import.py
@@ -25,6 +25,92 @@ def force_19yo_dms_import_cli(dry_run: bool = True) -> None:
     force_19yo_dms_import(dry_run=dry_run)
 
 
+def force_activation(ids):
+    for user_id in ids:
+        user = users_models.User.query.get(user_id)
+        fraud_check = (
+            fraud_models.BeneficiaryFraudCheck.query.filter_by(user=user, type=fraud_models.FraudCheckType.DMS)
+            .order_by(fraud_models.BeneficiaryFraudCheck.id.desc())
+            .first()
+        )
+        content = fraud_check.source_data()
+        fraud_api.create_honor_statement_fraud_check(
+            user, "honor statement contained in DMS application", users_models.EligibilityType.AGE18
+        )
+        try:
+            subscription_api.on_successful_application(
+                user,
+                fraud_check.source_data(),
+                BeneficiaryImportSources.demarches_simplifiees,
+                users_models.EligibilityType.AGE18,
+                application_id=content.application_id,
+                source_id=content.procedure_id,
+                third_party_id=fraud_check.thirdPartyId,
+            )
+        except Exception:  # pylint: disable=broad-except
+            logger.warning("Cannot activate user %d", user.id, exc_info=True)
+            continue
+        user.comment = (
+            "Inscription forcée : Utilisateur ayant 18 ans lors de son inscription et 19 ans lors de la validation DMS"
+        )
+        db.session.add(user)
+        db.session.commit()
+        logger.info("User %s is now activated", user.id)
+
+
+def find_dup_id(ids):
+    for user_id in ids:
+        user = users_models.User.query.get(user_id)
+        fraud_check = (
+            fraud_models.BeneficiaryFraudCheck.query.filter_by(user=user, type=fraud_models.FraudCheckType.DMS)
+            .order_by(fraud_models.BeneficiaryFraudCheck.id.desc())
+            .first()
+        )
+        content = fraud_check.source_data()
+        if (
+            fraud_api.duplicate_id_piece_number_fraud_item(user, content.id_piece_number).status
+            != fraud_models.FraudStatus.OK
+        ):
+            print("dupp--------", user.id)
+
+
+def debug(dry_run: bool = True) -> None:
+    users = (
+        users_models.User.query.join(fraud_models.BeneficiaryFraudCheck)
+        .join(fraud_models.BeneficiaryFraudResult)
+        .filter(
+            fraud_models.BeneficiaryFraudCheck.type == fraud_models.FraudCheckType.DMS,
+            fraud_models.BeneficiaryFraudResult.status == fraud_models.FraudStatus.KO,
+            users_models.User.has_beneficiary_role != True,
+        )
+    ).all()
+    to_activate = []
+    for user in users:
+        fraud_check = next(
+            check for check in user.beneficiaryFraudChecks if check.type == fraud_models.FraudCheckType.DMS
+        )
+        data = fraud_check.source_data()
+        if (
+            fraud_api._duplicate_user_fraud_item(data.first_name, data.last_name, data.birth_date, user.id).status
+            != fraud_models.FraudStatus.OK
+        ):
+            print("duplicate-----------", user.id)
+            continue
+        if not (
+            user.dateOfBirth
+            and user.age == 19
+            and users_utils.get_age_at_date(user.dateOfBirth, user.dateCreated) == 18
+        ):
+            print("not 18 at creation", user.id)
+            continue
+        for fraud_result in user.beneficiaryFraudResults:
+            if [code for code in fraud_result.reason_codes if code == fraud_models.FraudReasonCode.ALREADY_BENEFICIARY]:
+                to_activate.append(user.id)
+                break
+            print("user not activable", user.id)
+    return to_activate
+
+
 def force_19yo_dms_import(dry_run: bool = True) -> None:
     users = (
         users_models.User.query.join(fraud_models.BeneficiaryFraudCheck)
@@ -32,6 +118,7 @@ def force_19yo_dms_import(dry_run: bool = True) -> None:
         .filter(
             fraud_models.BeneficiaryFraudCheck.type == fraud_models.FraudCheckType.DMS,
             fraud_models.BeneficiaryFraudResult.status == fraud_models.FraudStatus.KO,
+            users_models.User.has_benefipciary_role != True,
         )
     )
     to_activate = []
