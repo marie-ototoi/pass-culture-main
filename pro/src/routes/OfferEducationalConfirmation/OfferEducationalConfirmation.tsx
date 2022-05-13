c692b50ab83c0380fc14b921af97c863a6b15f2a
MathildeDuboille <duboille.m@gmail.com>
2022-05-13 17:01:50 +0200

    (PC-15074)[PRO] feat: create adapter to get collective offer template on stock page

--- pro/src/routes/OfferEducationalConfirmation/OfferEducationalConfirmation.tsx
+++ pro/src/routes/OfferEducationalConfirmation/OfferEducationalConfirmation.tsx
@@ -4,19 +4,32 @@ import { useParams } from 'react-router-dom'
 import useNotification from 'components/hooks/useNotification'
 import Spinner from 'components/layout/Spinner'
 import {
+  extractOfferIdAndOfferTypeFromRouteParams,
+  getStockCollectiveOfferAdapter,
   getStockOfferAdapter,
   GetStockOfferSuccessPayload,
 } from 'core/OfferEducational'
 import OfferEducationalConfirmationScreen from 'screens/OfferEducationalConfirmation'
+import useActiveFeature from 'components/hooks/useActiveFeature'
+import { getStockCollectiveOfferTemplateAdapter } from 'core/OfferEducational/adapters/getStockCollectiveOfferTemplateAdapter'
 
 const OfferEducationalConfirmation = (): JSX.Element => {
-  const { offerId } = useParams<{ offerId: string }>()
+  const { offerId: offerIdFromParams } = useParams<{ offerId: string }>()
+  const { offerId, isShowcase } =
+    extractOfferIdAndOfferTypeFromRouteParams(offerIdFromParams)
   const [offer, setOffer] = useState<GetStockOfferSuccessPayload>()
   const notify = useNotification()
+  const enableIndividualAndCollectiveSeparation = useActiveFeature(
+    'ENABLE_INDIVIDUAL_AND_COLLECTIVE_OFFER_SEPARATION'
+  )
 
   useEffect(() => {
     const loadOffer = async () => {
-      const offerResponse = await getStockOfferAdapter(offerId)
+      const getOfferAdapter = enableIndividualAndCollectiveSeparation
+          ? isShowcase ? getStockCollectiveOfferTemplateAdapter : getStockCollectiveOfferAdapter
+          : getStockOfferAdapter
+
+      const offerResponse = await getOfferAdapter(offerId)
 
       if (!offerResponse.isOk) {
         return notify.error(offerResponse.message)
